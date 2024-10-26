85b99fe7f0da291f7c88d048dc003fd383ec7572
Acker <50650111+garlzr@users.noreply.github.com>
2024-10-26 21:07:03 +0800

    Create restart-cf.sh

---
+++ restart-cf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 无限循环
+while true; do
+    # 获取当前时间
+    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+
+    # 关闭所有匹配条件的 screen 会话
+    echo "[$timestamp] Closing detached screen sessions..."
+    screen -ls | grep Detached | grep cf | awk -F '[.]' '{print $1}' | xargs -I {} screen -S {} -X quit
+
+    # 启动新的 screen 会话并执行指定命令
+    echo "[$timestamp] Starting new screen session for cf..."
+    screen -dmS Quili bash -c 'cd ~/cf-clearance-scraper && npm start'
+
+    # 等待10分钟
+    echo "[$timestamp] Waiting for 30 minutes..."
+    sleep 600
+
+    # 更新时间戳
+    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "[$timestamp] Cycle completed. Restarting process..."
+done
